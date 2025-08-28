6b5a02dc7475fddfebd40d3869b7a7c6a5c6f88e
GraysonSalem <gsalem2@huskers.unl.edu>
2025-08-28 10:14:55 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Grayson Salem
+ *
+ * Date: 2025/08/13
+ *
+ * A simple program that prints my name
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char**argv) {
+
+    printf("Grayson Salem\n");
+
+    return 0;
+}
+
